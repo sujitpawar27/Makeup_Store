f43ee3befca6538c5d4f7d3ee41d3e6d1d26171a
sujitpawar <sujitpawar074@gmail.com>
2024-11-08 11:47:44 +0530

    added files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 import './navbar.css';
